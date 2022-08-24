1b5fc51a890d736bd894b0ff9dc91a48d1577317
李建彤 <1791224624@qq.com>
2022-08-24 15:15:15 +0800

    update

--- 每日一题/src/Main.java
+++ 每日一题/src/Main.java
@@ -1,12 +1,16 @@
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Scanner;
 
 /**
  * @author 李建彤
  */
 public class Main {
     public static void main(String[] args) {
-        Day62_ColorBorder te = new Day62_ColorBorder();
-        te.colorBorder(new int[][]{{1, 1,1}, {1, 1, 1}, {1, 1, 1}}, 1, 1, 2);
+//        Day62_ColorBorder te = new Day62_ColorBorder();
+//        te.colorBorder(new int[][]{{1, 1,1}, {1, 1, 1}, {1, 1, 1}}, 1, 1, 2);
+        String a = "aa";
+        String b = "bb";
+        System.out.println(a == b);
     }
 }
