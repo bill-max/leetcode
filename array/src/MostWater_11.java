612d9dd01581da174acfd9b6729c6cd9543db21d
bill <1791224624@qq.com>
2022-10-06 15:46:09 +0800

    add offer_04

---
+++ array/src/MostWater_11.java
@@ -0,0 +1,12 @@
+public class MostWater_11 {
+    class Solution {
+        public int maxArea(int[] height) {
+            int left=0,right=1;
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
